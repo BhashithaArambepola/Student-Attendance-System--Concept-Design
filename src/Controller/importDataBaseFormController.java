d8f15c9b135e251b900aa70d93c9573398a0b148
bhashitha Arambepola <barambepola@gmail.com>
2022-01-29 12:47:00 +0530

    Import Data Base Form Finalize

--- src/Controller/importDataBaseFormController.java
+++ src/Controller/importDataBaseFormController.java
@@ -1,4 +1,52 @@
 package Controller;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class importDataBaseFormController {
+    public RadioButton rdoRestore;
+    public ToggleGroup abc;
+    public TextField txtBrowse;
+    public Button btnBrowse;
+    public RadioButton rdoFirstTime;
+    public Button btnOK;
+    private SimpleObjectProperty<File> fileProperty;
+
+
+    public void initialize() {
+        txtBrowse.setEditable(false);
+    }
+
+    public void initialFileProperty(SimpleObjectProperty<File> fileProperty) {
+        this.fileProperty = fileProperty;
+
+    }
+
+    public void btnBrowse_OnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a BackupFile");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Backup file", "*.sipwinBackup"));
+        File file = fileChooser.showOpenDialog(btnOK.getScene().getWindow());
+        txtBrowse.setText(file != null ? file.getAbsolutePath() : "");
+        fileProperty.setValue(file);
+
+    }
+
+    public void btnOK_OnAction(ActionEvent actionEvent) {
+
+        if (rdoFirstTime.isSelected()) {
+            fileProperty.setValue(null);
+        }
+        ((Stage) (btnOK.getScene().getWindow())).close();
+    }
+
+
 }
--- src/Controller/splashScreenController.java
+++ src/Controller/splashScreenController.java
@@ -16,48 +16,49 @@ import java.sql.SQLException;
 public class splashScreenController {
     public Label lblStatus;
 
-    public void initialize(){
+    public void initialize() {
         establishDatBaseConnection();
     }
 
-    private void establishDatBaseConnection(){
+    private void establishDatBaseConnection() {
         lblStatus.setText("Establish Data Base Connection...");
-new Thread(()->{
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sipwin", "root", "78527852");
+        new Thread(() -> {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sipwin", "root", "78527852");
 
-    } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-    } catch (SQLException e) {
-        if (e.getSQLState().equals("42000")){
-            Platform.runLater(this::loadImportDataBaseForm);
-        }
-        e.printStackTrace();
-    }
-}).start();
-
-        }
-        private void loadImportDataBaseForm(){
-           try {
-               Stage stage = new Stage();
-               AnchorPane root = FXMLLoader.load(this.getClass().getResource("/View/importDataBaseForm.fxml"));
-               Scene scene=new Scene(root);
-               stage.setScene(scene);
-               stage.centerOnScreen();
-               stage.setResizable(false);
-               stage.setTitle("First Time Boot");
-               stage.initModality(Modality.APPLICATION_MODAL);
-               stage.initOwner(lblStatus.getScene().getWindow());
-               stage.sizeToScene();
-               stage.show();
-           }catch (IOException e){
-               e.printStackTrace();
-           }
-
-        }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                if (e.getSQLState().equals("42000")) {
+                    Platform.runLater(this::loadImportDataBaseForm);
+                }
+                e.printStackTrace();
+            }
+        }).start();
 
     }
+
+    private void loadImportDataBaseForm() {
+        try {
+            Stage stage = new Stage();
+            AnchorPane root = FXMLLoader.load(this.getClass().getResource("/View/importDataBaseForm.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.setResizable(false);
+            stage.setTitle("First Time Boot");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(lblStatus.getScene().getWindow());
+            stage.sizeToScene();
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
 
 
 
